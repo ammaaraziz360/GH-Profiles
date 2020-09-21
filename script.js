0af0443779836200a792bc537acf9afab059d4c3
Ammaar Mohammed <ammaaraziz360@gmail.com>
2020-09-20 22:40:55 -0500

    updated visuals
    lets user know if user isnt found

--- script.js
+++ script.js
@@ -8,14 +8,29 @@ function changeUsername(){
   username = document.getElementById("username").value;
   staticUrl = baseURL.concat(username)
   console.log(staticUrl)
+
+  document.getElementById("Name").innerHTML = "Error: User not found"
+  document.getElementById("pfp").src = ""
+  document.getElementById("fail").className = "far fa-frown"
+  document.getElementById("created").innerHTML = ""
+  document.getElementById("updated").innerHTML = ""
+
   $.getJSON(staticUrl, function(data) {
     console.log("This is an example of a static JSON file being served by a web server.")
     console.log(data);
     jsonobj = data
+    console.log(jsonobj.message);
+  
     document.getElementById("Name").innerHTML = jsonobj.name
+
+    document.getElementById("fail").classList.remove("far")
+    document.getElementById("fail").classList.remove("fa-frown")
+
     document.getElementById("pfp").src = jsonobj.avatar_url
     document.getElementById("created").innerHTML = jsonobj.created_at
     document.getElementById("updated").innerHTML = jsonobj.updated_at
+    
+      
 
   });
 }
